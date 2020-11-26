29d318b7b17bbf83542e81606622ea7133aea5ea
leedahw <71396271+leedahw@users.noreply.github.com>
2020-11-25 22:18:26 -0500

    logo img animation working

--- homepage.php
+++ homepage.php
@@ -5,6 +5,8 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="./css/main.css" />
+    <link rel= "main.css" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;700;900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="home page/ dashboard for IMM News Network">
